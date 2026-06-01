8ecd05d800cb6b38c522f9faf6bec67cea41d2ff
iTovarr <13m.tovar.m@gmail.com>
2026-05-31 20:15:10 -0500

    feat: implementar getTotal para pasar prueba de carrito vacío (R1)

--- src/main/java/pe/tecsup/carrito/CartService.java
+++ src/main/java/pe/tecsup/carrito/CartService.java
@@ -1,4 +1,7 @@
 package pe.tecsup.carrito;
 
 public class CartService {
+    public double getTotal() {
+        return 0.0;
+    }
 }
--- src/test/java/pe/tecsup/carrito/CartServiceTest.java
+++ src/test/java/pe/tecsup/carrito/CartServiceTest.java
@@ -5,5 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("CartService - reglas de negocio del carrito")
 class CartServiceTest {
-    // Aquí escribirás las pruebas
+    private CartService cart;
+
+    @BeforeEach
+    void setUp() {
+        cart = new CartService();
+    }
+
+    @Test
+    @DisplayName("Carrito vacío: total debe ser 0.0")
+    void getTotal_carritoVacio_retornaCero() {
+        // Arrange — hecho en setUp()
+
+        // Act
+        double total = cart.getTotal();
+
+        // Assert
+        assertEquals(0.0, total);
+    }
+
 }
